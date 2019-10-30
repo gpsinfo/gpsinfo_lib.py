9ac158bd71762fc0761c7bf4aef0cd5ccde04b75
Simon Flöry <simon.floery@rechenraum.com>
2019-10-30 14:59:05 +0100

    huuuh, that's cool

--- src/gpsinfo.py
+++ src/gpsinfo.py
@@ -10,7 +10,7 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 gpsinfo is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+but WITHOUT ANY WARRANTY without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
@@ -29,6 +29,8 @@ along with gpsinfo. If not, see <http://www.gnu.org/licenses/>.
 import xml.etree.ElementTree as xmlET
 import sys
 import urllib.request
+# sudo apt install python3-gdal
+import gdal
 
 # We support python3 only
 assert sys.version_info > (3, 0)
@@ -39,18 +41,40 @@ assert sys.version_info > (3, 0)
 #
 ################################################################################
 
-class ServiceInfo:
+class Service:
+	
+	#---------------------------------------------------------------------------
 	
 	# Constructor
 	def __init__(self):
+		self.__isConnected = False
 		self.__xmlNamespace = {
 			'wmts' : 'http://www.opengis.net/wmts/1.0',
 			'ows' : 'http://www.opengis.net/ows/1.1'
 		}
+	
+	#---------------------------------------------------------------------------
+		
+	# \brief Checks is a connection could be established successfully
+	#
+	# This basically means the class was successfully initialized
+	#
+	# \return True, if connection was successful, false otherwise.
+	def isConnected(self):
+		return self.__isConnected
 
-	# baseurl is URL to XML file
+	#---------------------------------------------------------------------------
+
+	# \brief Establish connection to a gpsinfo service
+	#
+	# A successful call to this method is mandatory.
+	#
+	# \param baseurl Full URL to XML file. For local files, prefix with file://
+	#
+	# \return String in case of error, nothing in case of success
 	def connect(self, baseurl):
 		
+		self.__isConnected = False
 		self.__baseurl = baseurl
 		
 		# Download XML file
@@ -73,18 +97,46 @@ class ServiceInfo:
 		self.__layers = []
 		for layerNode in self.__xmlRoot.findall('wmts:Contents/wmts:Layer/ows:Title', self.__xmlNamespace):
 			self.__layers.append(layerNode.text)
+			
+		self.__isConnected = True
 		
+	#---------------------------------------------------------------------------
+	
+	# \brief Getter for baseurl property
+	#
+	# \return baseurl of connected service, e.g. URL of XML file
 	def baseurl(self):
+		if not self.isConnected() : return 'ERROR: You need to successfully connect to a service first.'
 		return self.__baseurl
 		
+	#---------------------------------------------------------------------------
+		
+	# \brief Get layers of connected service
+	#
+	# \return String list of layers of connected service
 	def layers(self):
+		if not self.isConnected() : return 'ERROR: You need to successfully connect to a service first.'
 		return self.__layers
 		
+	#---------------------------------------------------------------------------
+			
+	# \brief Getter for root of XML element tree of the service's XML file
+	#
+	# \return Root of XML element tree of the service's XML file
 	def xmlRoot(self):
+		if not self.isConnected() : return 'ERROR: You need to successfully connect to a service first.'
 		return self.__xmlRoot
 		
+	#---------------------------------------------------------------------------
+			
+	# \brief Getter for XML namespace
+	#
+	# \return XML namespace
 	def xmlNamespace(self):
+		if not self.isConnected() : return 'ERROR: You need to successfully connect to a service first.'
 		return self.__xmlNamespace
+		
+	#---------------------------------------------------------------------------
 		
 ################################################################################
 #
@@ -92,19 +144,130 @@ class ServiceInfo:
 #
 ################################################################################
 
-class LayerInfo:
+class Layer:
+	
+	#---------------------------------------------------------------------------
 	
 	# Constructor
 	def __init__(self):
-		pass
+		self.__isConnected = False
 		
-	def connect(self, serviceInfo, layerName):
-		layerNode = serviceInfo.xmlRoot().findall("./wmts:Contents/wmts:Layer[ows:Title='" + layerName + "']", serviceInfo.xmlNamespace())
+	#---------------------------------------------------------------------------
 		
-	def value(self, method, x, y, value):
-		value = 4
-		return ''
+	# \brief Checks is a connection could be established successfully
+	#
+	# This basically means the class was successfully initialized
+	#
+	# \return True, if connection was successful, false otherwise.
+	def isConnected(self):
+		return self.__isConnected
+
+	#---------------------------------------------------------------------------
 		
-	def values(self, xLowerLeft, yLowerLeft, xUpperRight, yUpperRight, values):
-		return ''
+	def connect(self, service, layerName):
+		self.__isConnected = False
+		if not service.isConnected() : 
+			return 'ERROR: You need to pass a successfully connected service.'
+			
+		layerNode = service.xmlRoot().findall("./wmts:Contents/wmts:Layer[ows:Title='" + layerName + "']", service.xmlNamespace())
+		if (len(layerNode) == 0) :
+			return 'ERROR Failed to find layer with title ' + layerName + '.'
+		elif (len(layerNode) != 1) :
+			return 'ERROR Failed to find unique layer with title ' + layerName + '.'
+		
+		self.__layerInfo = { }
+		self.__layerInfo['URLFormat'] = layerNode[0].find('wmts:ResourceURL', service.xmlNamespace()).attrib['format']
+		self.__layerInfo['URLTemplate'] = layerNode[0].find('wmts:ResourceURL', service.xmlNamespace()).attrib['template']
+		
+		tileMatrixSet = layerNode[0].find("wmts:TileMatrixSetLink/wmts:TileMatrixSet", service.xmlNamespace()).text
+		tileMatrixSetNode = service.xmlRoot().find("./wmts:Contents/wmts:TileMatrixSet[ows:Identifier='" + tileMatrixSet + "']", service.xmlNamespace())
+		
+		topLeftCorner = tileMatrixSetNode.find('wmts:TileMatrix/wmts:TopLeftCorner', service.xmlNamespace()).text.split()
+		self.__layerInfo['topLeftCornerX'] = float(topLeftCorner[0])
+		self.__layerInfo['topLeftCornerY'] = float(topLeftCorner[1])
+		self.__layerInfo['tileWidth'] = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:TileWidth', service.xmlNamespace()).text)
+		self.__layerInfo['tileHeight'] = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:TileHeight', service.xmlNamespace()).text)
+		self.__layerInfo['nrTilesX'] = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:MatrixWidth', service.xmlNamespace()).text)
+		self.__layerInfo['nrTilesY'] = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:MatrixHeight', service.xmlNamespace()).text)
+		self.__layerInfo['cellsize'] = float(tileMatrixSetNode.find('wmts:TileMatrix/wmts:ScaleDenominator', service.xmlNamespace()).text) * 0.00028
+				
+		self.__isConnected = True
+	
+	#---------------------------------------------------------------------------
+	
+	def __loadTileData(self, tileRowIndex, tileColIndex) :
+		# Build URL
+		url = self.__layerInfo['URLTemplate'].replace('{TileCol}', str(tileColIndex)).replace('{TileRow}', str(tileRowIndex))
+		
+		# GDAL's virtual file system: 
+		#	https://gdal.org/user/virtual_file_systems.html
+		if not url.startswith('file://') : 
+			url = '/vsicurl/' + url
+		else :
+			# We must strip the 'file://' prefix for gdal.Open
+			url = url[7:]
+		if self.__layerInfo['URLFormat'] == 'application/zip' :
+			url = '/vsizip/' + url
+		
+		# print('Loading ' + url + ' ...')
+		
+		# GDAL and python: 
+		#	- https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html
+		#	- https://automating-gis-processes.github.io/2016/Lesson7-read-raster.html
+		gdal_dataset = gdal.Open(url)
+		if gdal_dataset is None:
+			return 'ERROR Failed to open ' + url + '.'
+					
+		# read data as numpy array
+		array = gdal_dataset.ReadAsArray()
+		
+		# Close the dataset
+		gdal_dataset = None
+		
+		return array
+	
+	#---------------------------------------------------------------------------
+	
+	def __convertCoords2Idx(self, method, coordsX, coordsY) :
+		globalColIndex = int(round((coordsX - self.__layerInfo['topLeftCornerX']) / self.__layerInfo['cellsize']))
+		globalRowIndex = int(round((self.__layerInfo['topLeftCornerY'] - coordsY) / self.__layerInfo['cellsize']))
+
+		if (globalColIndex < 0) | (globalRowIndex < 0) :
+			return 'ERROR: Query point out of bounds.'
+		
+		# The wanted value is in tile (tileRowIndex, tileColIndex) at 
+		# (localRowIndex, localColIndex)
+		tileColIndex, localColIndex = divmod(globalColIndex, self.__layerInfo['tileWidth'])
+		tileRowIndex, localRowIndex = divmod(globalRowIndex, self.__layerInfo['tileHeight'])
+
+		if (tileColIndex >= self.__layerInfo['nrTilesX']) | (tileRowIndex >= self.__layerInfo['nrTilesY']) :
+			return 'ERROR: Query point out of bounds.'
+		
+		return { 
+			'tileRowIndex' : tileRowIndex, 
+			'tileColIndex' : tileColIndex, 
+			'localRowIndex' : localRowIndex,
+			'localColIndex' : localColIndex
+		}
+
+	#---------------------------------------------------------------------------
+		
+	def value(self, method, x, y) :
+		if not self.isConnected() : return 'ERROR: You need to successfully connect a layer first.'
+		
+		inds = self.__convertCoords2Idx(method, x,y)
+		if isinstance(inds, str) : return inds
+		if not isinstance(inds, dict) : return 'Error: Unexpected conversion result.'
+				
+		data = self.__loadTileData(inds['tileRowIndex'], inds['tileColIndex'])
+		if isinstance(data, str) : return data
+		
+		return data[inds['localRowIndex'],inds['localColIndex']]
+			
+	#---------------------------------------------------------------------------
+		
+	def values(self, xLowerLeft, yLowerLeft, xUpperRight, yUpperRight):
+		if not self.isConnected() : return 'ERROR: You need to successfully connect a layer first.'
+		return 'ERROR: not implemented yet'
         
+	#---------------------------------------------------------------------------
--- src/gpsinfo_client.py
+++ src/gpsinfo_client.py
@@ -27,13 +27,13 @@ along with gpsinfo. If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 # INPUT_filenameXML = 'file:///home/rechenraum/Base/data/gpsinfo/20191024/gpsinfoWMTSCapabilities.xml'
-# INPUT_layerTitle = 'AUSTRIA DGM 10x10m'
+# INPUT_layerName = 'AUSTRIA DGM 10x10m'
 
 # INPUT_filenameXML = 'file:///home/simon/Base/data/gpsinfo/20191025/gpsinfoWMTSCapabilities.xml'
-# INPUT_layerTitle = 'gpsinfo Test Layer'
+# INPUT_layerName = 'gpsinfo Test Layer'
 
 INPUT_filenameXML = 'http://gpsinfo.org/service_wmts/gpsinfoWMTSCapabilities.xml'
-INPUT_layerTitle = 'AT_OGD_DHM_LAMB_10M_ELEVATION_COMPRESSED'
+INPUT_layerName = 'AT_OGD_DHM_LAMB_10M_ELEVATION_COMPRESSED'
 
 ################################################################################
 #
@@ -50,43 +50,6 @@ import gpsinfo
 
 ################################################################################
 #
-# \brief Download/open tile via GDAL and returns its data as numpy array
-#
-# \param layerURLTemplate URL template as defined in the XML file
-# \param tileRowIndex Row index of tile
-# \param tileColIndex Column index of tile
-#
-# \return Numpy array storing the tile data
-# 
-################################################################################
-
-def loadTileData(layerURLTempalte, tileRowIndex, tileColIndex) :
-	# Build URL
-	url = layerURLTemplate.replace('{TileCol}', str(tileColIndex)).replace('{TileRow}', str(tileRowIndex))
-	# DEBUGGING ONLY (start)
-	#url = url.replace('c/', '/home/rechenraum/Base/data/gpsinfo/20191024/')
-	# DEBUGGING ONLY (end)
-	print('Loading ' + url + ' ...')
-
-	# GDAL's virtual file system: https://gdal.org/user/virtual_file_systems.html
-	# GDAL and python: 
-	#	- https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html
-	#	- https://automating-gis-processes.github.io/2016/Lesson7-read-raster.html
-	gdal_dataset = gdal.Open(url)
-	if gdal_dataset is None:
-		print('ERROR Failed to open ' + url + '.')
-		sys.exit()
-	
-	# read data as numpy array
-	array = gdal_dataset.ReadAsArray();
-	
-	# Close the dataset
-	gdal_dataset = None
-	
-	return array
-
-################################################################################
-#
 # main
 #
 ################################################################################
@@ -99,95 +62,36 @@ print('(c) 2019 Rechenraum GmbH (office@rechenraum.com).')
 print('================================================================================')
 print('')
 
-si = gpsinfo.ServiceInfo()
-si.connect(INPUT_filenameXML)
-for layer in si.layers():
+print('Connecting to ' + INPUT_filenameXML + ' ...')
+gpsinfo_service = gpsinfo.Service()
+gpsinfo_service.connect(INPUT_filenameXML)
+print('Available layers: ')
+for layer in gpsinfo_service.layers():
     print('\t' + layer)
-    
-sys.exit()
+print('')
 
-################################################################################
-#
-# parse XML file
-#
-################################################################################
-
-# https://docs.python.org/3/library/xml.etree.elementtree.html
-xmlNamespace = {
-			'wmts' : 'http://www.opengis.net/wmts/1.0',
-			'ows' : 'http://www.opengis.net/ows/1.1'
-		}
-
-xmlRoot = xmlET.parse(INPUT_filenameXML).getroot()
-
-print('Available layers:')
-for layerNode in xmlRoot.findall('wmts:Contents/wmts:Layer/ows:Title', xmlNamespace):
-    print('\t' + layerNode.text)
-
-sys.exit()
-
-################################################################################
-#
-# collect information about requested layer
-#
-################################################################################
-
-layerNode = xmlRoot.findall("./wmts:Contents/wmts:Layer[ows:Title='" + INPUT_layerTitle + "']", xmlNamespace)
-if (len(layerNode) == 0) :
-	print('ERROR Failed to find layer with title ' + INPUT_layerTitle + '.')
-	sys.exit()
-elif (len(layerNode) != 1) :
-	print('ERROR Failed to find unique layer with title ' + INPUT_layerTitle + '.')
+gpsinfo_layer = gpsinfo.Layer()
+error = gpsinfo_layer.connect(gpsinfo_service, INPUT_layerName)
+if isinstance(error, str) : 
+	print(error)
 	sys.exit()
 
-layerURLFormat = layerNode[0].find('wmts:ResourceURL', xmlNamespace).attrib['format']
-layerURLTemplate = layerNode[0].find('wmts:ResourceURL', xmlNamespace).attrib['template']
-
-tileMatrixSet = layerNode[0].find("wmts:TileMatrixSetLink/wmts:TileMatrixSet", xmlNamespace).text
-tileMatrixSetNode = xmlRoot.find("./wmts:Contents/wmts:TileMatrixSet[ows:Identifier='" + tileMatrixSet + "']", xmlNamespace)
-
-topLeftCorner = tileMatrixSetNode.find('wmts:TileMatrix/wmts:TopLeftCorner', xmlNamespace).text.split()
-topLeftCornerX = float(topLeftCorner[0])
-topLeftCornerY = float(topLeftCorner[1])
-tileWidth = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:TileWidth', xmlNamespace).text)
-tileHeight = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:TileHeight', xmlNamespace).text)
-nrTilesX = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:MatrixWidth', xmlNamespace).text)
-nrTilesY = int(tileMatrixSetNode.find('wmts:TileMatrix/wmts:MatrixHeight', xmlNamespace).text)
-cellsize = float(tileMatrixSetNode.find('wmts:TileMatrix/wmts:ScaleDenominator', xmlNamespace).text) * 0.00028
-
-################################################################################
-#
-# compute tile index for a specific query point
-#
-################################################################################
-
-INPUT_qX = topLeftCornerX+100
-INPUT_qY = topLeftCornerY-200
-
-globalColIndex = int(round((INPUT_qX - topLeftCornerX) / cellsize));
-globalRowIndex = int(round((topLeftCornerY - INPUT_qY) / cellsize));
-
-if (globalColIndex < 0) | (globalRowIndex < 0) :
-	print('ERROR Query point out of bounds.')
+# height = gpsinfo_layer.value('undefined', 152803.0, 258808.0)
+height = gpsinfo_layer.value('undefined', 675392, 432848)
+# height = gpsinfo_layer.value('undefined', 125250, 371779)
+if isinstance(height, str) :
+	print(height)
 	sys.exit()
-
-# The wanted value is in tile (tileRowIndex, tileColIndex) at 
-# (localRowIndex, localColIndex)
-tileColIndex, localColIndex = divmod(globalColIndex, tileWidth)
-tileRowIndex, localRowIndex = divmod(globalRowIndex, tileHeight)
-
-if (tileColIndex >= nrTilesX) | (tileRowIndex >= nrTilesY) :
-	print('ERROR Query point out of bounds.')
-	sys.exit()
-
-# print(str(tileRowIndex) + ', ' + str(tileColIndex))
-# print(str(localColIndex) + ', ' + str(localRowIndex))
-
-data = loadTileData(layerURLTemplate, tileRowIndex, tileColIndex)
-print(str(data[localRowIndex,localColIndex]))
-
-sys.exit()
-
-
-
 	
+print('Elevation = ' + str(height))
+print('')
+
+# as short as it gets without any error handling
+service = gpsinfo.Service();
+service.connect('http://gpsinfo.org/service_wmts/gpsinfoWMTSCapabilities.xml')
+layer = gpsinfo.Layer()
+layer.connect(service, 'AT_OGD_DHM_LAMB_10M_ELEVATION_COMPRESSED')
+print('Elevation = ' + str(layer.value('undefined', 675392, 432848)))
+print('')
+
+    
